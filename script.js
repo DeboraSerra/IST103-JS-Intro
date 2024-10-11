b6d944ce3a0c52d4e320fdf4b3ddfd190058656f
Débora Serra <debora.r.serra@gmail.com>
2024-10-10 18:12:30 -0700

    feat: finish activity 3

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //alert("Hello world");
-/*const name = "Debora";
+const personName = "Debora";
 const age = 32;
 const isCanadian = false;
 const nullVar = null;
 let undefinedVar;
 
-console.log("Person name is ", name);
+console.log("Person name is ", personName);
 console.log("Person age is ", age);
 console.log("Person is Canadian ", isCanadian);
 console.log("null variable ", nullVar);
-console.log("undefined variable ", undefinedVar);*/
+console.log("undefined variable ", undefinedVar);
 
-const toDoArray = ["see northern lights!!", "read", "study"];
+const toDoArray = ["see northern lights!!", "read", "study", "work"];
 
 const phone = {
   brand: "apple",
@@ -19,3 +19,6 @@ const phone = {
   battery: 90,
   os: "iOS 17.6.1",
 };
+
+console.log("My to do list is ", toDoArray);
+console.log("My phone model is ", phone.model);
