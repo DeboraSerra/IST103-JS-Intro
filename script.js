9ffaef468fc579174517a40bdc30e0438f6aa796
Débora Serra <debora.r.serra@gmail.com>
2024-10-10 18:08:14 -0700

    feat: add to do array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //alert("Hello world");
-const name = "Debora";
+/*const name = "Debora";
 const age = 32;
 const isCanadian = false;
 const nullVar = null;
@@ -9,4 +9,6 @@ console.log("Person name is ", name);
 console.log("Person age is ", age);
 console.log("Person is Canadian ", isCanadian);
 console.log("null variable ", nullVar);
-console.log("undefined variable ", undefinedVar);
+console.log("undefined variable ", undefinedVar);*/
+
+const toDoArray = ["see northern lights!!", "read", "study"]
