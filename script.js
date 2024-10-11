ebad793ec191ce36baec8c21e9bd183b85d28c6c
Débora Serra <debora.r.serra@gmail.com>
2024-10-10 18:06:24 -0700

    feat: add activity 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Hello world");
+//alert("Hello world");
+const name = "Debora";
+const age = 32;
+const isCanadian = false;
+const nullVar = null;
+let undefinedVar;
+
+console.log("Person name is ", name);
+console.log("Person age is ", age);
+console.log("Person is Canadian ", isCanadian);
+console.log("null variable ", nullVar);
+console.log("undefined variable ", undefinedVar);
